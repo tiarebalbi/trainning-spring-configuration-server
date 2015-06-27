4bdf80cdd0d2670ed87bfc20128d185a1d1f283a
Tiare Balbi Bonamini <me@tiarebalbi.com>
2015-06-27 14:43:14 -0300

    Configuracao

---
+++ src/main/java/com/tiarebalbi/trainning/spring/CloudConfigurationServerApplication.java
@@ -0,0 +1,12 @@
+package com.tiarebalbi.trainning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigurationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigurationServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tiarebalbi/trainning/spring/CloudConfigurationServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tiarebalbi.trainning.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CloudConfigurationServerApplication.class)
+public class CloudConfigurationServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
